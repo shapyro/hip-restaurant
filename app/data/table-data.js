78c0dd6dfdd344d9a826ff17bc9de5d894ee8bb2
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-02-04 13:03:55 -0600

    more skeleton work

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+  {
+    customerName: 'user1',
+    customerEmail: 'bad@ss.com',
+    userName: 'abadassusername',
+    phoneNumber: '000-000-0000'
+  }
+]
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+  {
+    customerName: 'user2',
+    customerEmail: 'bad@ss1.com',
+    userName: 'user2userName',
+    phoneNumber: '000-000-0000'
+  }
+];
+
+module.exports = waitingArray;
